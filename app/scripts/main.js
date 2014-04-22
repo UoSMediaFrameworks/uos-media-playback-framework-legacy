b31fb74b160702da08eda30a8769992bd1134c04
Matt Schick <matt@schickm.com>
2014-04-21 20:17:19 -0500

    preview functionality for editor page. closes #2 closes #3

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,51 @@
 'use strict';
 
+var mediaScenePlayer = (function() {
+    var methods = {
+        showImage: function(url) {
+            var el = this.el;
+            var scope = this;
+            this.preloadImage(url, function(img) {
+                // put it off screen
+                var imgEl = $(img);
+                el.empty().append(imgEl);
+                scope.animateImage(imgEl);
+            });
+        },
+
+        animateImage: function(img) {
+            // random start position
+            img.css({
+                left: Math.random() * (this.elWidth - img.width()),
+                top: Math.random() * (this.elHeight - img.height())
+            });
+
+            // and show
+            TweenLite.to(img, 2, {opacity: 1});
+        },
+
+        preloadImage: function(url, callback) {
+            var img = new Image();
+            img.onload = function() {
+                callback(img);
+            };
+
+            img.src = url;
+        }
+    };
+
+    var constructor = function(el) {
+        var instance = Object.create(methods);
+        instance.el = el;
+        instance.elWidth = el.width();
+        instance.elHeight = el.height();
+
+        return instance;
+    };
+
+    return constructor;
+}());
+
 var MediaSceneEditor = Ractive.extend({
     addMediaObject: function() {
         this.get('newTags');
@@ -9,6 +55,7 @@ var MediaSceneEditor = Ractive.extend({
     init: function(options) {
         this.mediaScene = options.mediaScene;
         this.set('scene', this.mediaScene.scene);
+        this.mediaPlayer = options.mediaPlayer;
 
         this.on({
             newMediaObject: function(event) {
@@ -18,8 +65,8 @@ var MediaSceneEditor = Ractive.extend({
                         type: 'image',
                         mimeType: 'image/jpeg',
                         url: this.get('newUrl'),
-                        anmiationIn: 'default',
-                        anmiationOut: 'default',
+                        animiationIn: 'default',
+                        animiationOut: 'default',
                         cachePolicy: 'default',
                         tags: this.get('newTags')
                     }
@@ -27,12 +74,17 @@ var MediaSceneEditor = Ractive.extend({
 
                 this.set({newUrl: '', newTags: ''});
 
-
                 event.original.preventDefault();
             },
 
             preview: function(event) {
-                console.log(this.get(event.keypath));
+                this.mediaPlayer.showImage(this.get(event.keypath).mediaObject.url);
+            },
+
+            displayScene: function(event) {
+                var key = this.mediaScene.name;
+                localStorage[key] = JSON.stringify(this.mediaScene);
+                window.open('scenePlayer.html?scene=' + key);
             }
         });
     }
@@ -41,6 +93,7 @@ var MediaSceneEditor = Ractive.extend({
 new MediaSceneEditor({
     el: 'editor',
     template: '#media-scene-editor',
+    mediaPlayer: mediaScenePlayer($('#canvas')),
     mediaScene: {
         version: '1',
         name: 'scene1',
@@ -72,3 +125,6 @@ new MediaSceneEditor({
         ]
     }
 });
+
+
+
