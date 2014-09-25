2932d8ab66eecfa8a472a7887467f28db2f67449
Matt Schick <matt@schickm.com>
2014-09-25 11:02:35 -0500

    pull out old code

--- app/scripts/scene.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var player = mediaScenePlayer($('body'), loadMediaScene());
-
-$('.edit-scene').attr('href', 'index.html?scene=' + $.url().param('scene'));
-
-$('.tag-filter').blur(function() {
-    player.filterByTags(this.value);
-});
