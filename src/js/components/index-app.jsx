b07f3461197deeb85c8c539666695fb147957863
Matt Schick <matt@schickm.com>
2014-11-30 14:02:52 -0600

    display autologins

--- src/js/components/index-app.jsx
+++ src/js/components/index-app.jsx
@@ -4,7 +4,7 @@ var HubClient = require('../utils/HubClient');
 var LoginPage = require('./login/login-page.jsx');
 var SceneEditor = require('./scene-editor/scene-editor.jsx');
 
-HubClient.login('http://127.0.0.1:3000', 'kittens');
+//HubClient.login('http://127.0.0.1:3000', 'kittens');
 
 var App = React.createClass({
     
--- src/js/components/viewer-app.jsx
+++ src/js/components/viewer-app.jsx
@@ -4,7 +4,7 @@ var LoginPage = require('./login/login-page.jsx');
 var SceneViewer = require('./scene-viewer/scene-viewer.jsx');
 var HubClient = require('../utils/HubClient');
 
-HubClient.login('http://127.0.0.1:3000', 'kittens');
+//HubClient.login('http://127.0.0.1:3000', 'kittens');
 
 var ViewerApp = React.createClass({
 
