b8422ed60f1237cf72f28593a2aa7997a16f4c03
aaronphillips <aaronp261@gmail.com>
2016-06-14 22:37:59 +0100

    https://trello.com/c/hxXoqgpD/15-fix-error-log-on-screen
    Implementing new scene action dismissStatus, creating a new view action to be handled by the dispatcher.
     - The status message store already implemented an ActionType to remove a message from the store given its identifier (alertId)
    
    Updated the status alert react component to include an on click to dismiss the status box.
    - A small visible close icon has also been added into the status box ui so the user can see they can dismiss the status box.

--- src/js/actions/scene-actions.js
+++ src/js/actions/scene-actions.js
@@ -119,6 +119,14 @@ var SceneActions = {
         location.reload();
     },
 
+    dismissStatus: function(alertId) {
+        AppDispatcher.handleViewAction({
+            type: ActionTypes.STATUS_MESSAGE_REMOVE,
+            message: alertId,
+            id: alertId
+        });
+    },
+
     uploadAsset: function(sceneId, file) {
         var alertId = hat();
         AppDispatcher.handleViewAction({
--- src/js/components/status-alert.jsx
+++ src/js/components/status-alert.jsx
@@ -1,12 +1,19 @@
 var React = require('react');
+var SceneActions = require('../actions/scene-actions');
 
 var StatusAlert = React.createClass({
+
+    handleStatusClick: function(alertId) {
+        SceneActions.dismissStatus(alertId);
+    },
 
 	render: function() {
 		var klass = 'alert alert-' + this.props.state.state;
 		return (
-			<div className={klass}>
+			<div className={klass} onClick={() => this.handleStatusClick(this.props.state.id)}>
 				{this.props.state.message}
+
+                <span className="glyphicon glyphicon-remove"></span>
 			</div>
 		);
 	}
