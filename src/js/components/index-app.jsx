a48f35d39cc7798444fa79d9a96969b6578950d6
aaronphillips <aaronp261@gmail.com>
2016-07-19 13:33:08 +0100

    Creative commons added to the login screen. - Used the badge generator for creating the HTML.  Small change was made to be react.js friendly [style tags need to be replaced]

--- src/js/components/index-app.jsx
+++ src/js/components/index-app.jsx
@@ -20,7 +20,7 @@ function _getState () {
 }
 
 var App = React.createClass({
-    
+
     getInitialState: function() {
         return _getState();
     },
@@ -42,7 +42,7 @@ var App = React.createClass({
     handleLogout: function(event) {
         SceneActions.logout();
     },
-    
+
     render: function() {
         var sessionNav, nav;
 		//AJF: gets the groupID then passes as a parameter to get the group name. I tried a version of getShortGroupName that would accept no groupID and get it from the current session but it wasn't working @todo: fix this in connectioncache
@@ -56,7 +56,7 @@ var App = React.createClass({
 
         var statusAlerts = Object.keys(messages).map(function(name) {
             return <StatusAlert key={name} name={name} state={messages[name]} />;
-        });        
+        });
 
         return (
             <div className='app'>
@@ -64,21 +64,25 @@ var App = React.createClass({
                     {statusAlerts}
                 </div>
                 <div className='header'>
-                    {sessionNav}                      
+                    {sessionNav}
                     <a target='_blank' className='dos-donts' href='https://docs.google.com/document/d/1B25gvDRob576KPsgusEhhUY3GI_XF6guHIBpLPrn9U0/edit?usp=sharing'>
                         Do's &amp; Don'ts of Media Frameworks
-                    </a> 
+                    </a>
                     <h4 className='title'>Media Scene Editor</h4>
-					
+
                 </div>
                 <Loader message='Logging in...' loaded={! this.state.attemptingLogin}>
                     <RouteHandler key='handler' />
                 </Loader>
-				
-            </div>
-        );       
 
-        
+                <div className="footer navbar-fixed-bottom" style={{ padding: '5px' }}>
+                    <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons Licence" style={{ borderWidth:0 }} src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>
+                </div>
+
+            </div>
+        );
+
+
     }
 
 });
