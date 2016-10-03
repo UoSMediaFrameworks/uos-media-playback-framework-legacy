f0859838f7b06c7a79cd7995b8e5b4cde91170b5
aaronphillips <aaronp261@gmail.com>
2016-10-03 22:59:52 +0100

    Update the the monaco editor - working on handling the focusMediaObject and saving strategy TODOS - Handle errors from saving process with UI info - FocusMediaObject awaiting support via github issues about possible bug with regex for multi line search

--- src/js/components/scene-monaco-text-editor.jsx
+++ src/js/components/scene-monaco-text-editor.jsx
@@ -2,14 +2,15 @@
 
 var React = require('react');
 var MonacoEditor = require('react-monaco-editor').default;
+var SceneActions = require('../actions/scene-actions');
 var SceneList = require('./scene-list.jsx');
+var _ = require('lodash');
 
 var monacoEditor = undefined;
 
 var SceneMonacoTextEditor = React.createClass({
 
     getHumanReadableScene: function() {
-
         console.log("getHumanReadableScene: ", this.props);
 
         return this.props.scene; //TODO _.omit _id
@@ -25,33 +26,74 @@ var SceneMonacoTextEditor = React.createClass({
         return JSON.stringify(scene, null, 2);
     },
 
-    getInitialState: function() {
-
-        console.log("getInitialState done");
-
+    getState: function() {
         return {
-            code: this.getSceneString()
+            scene: this.props.scene,
+            code: this.getSceneString(),
+            focusedMediaObject: this.props.focusedMediaObject
         };
     },
 
-    _onChange: function() {
-        console.log("SceneMonacoTextEditor - _onChange");
+    getInitialState: function() {
+        console.log("getInitialState done");
 
-        this.setState(this.getInitialState());
-        // monacoEditor.smartIndent();
+        return this.getState();
     },
 
-    onChange: function(newValue, e) {
-        console.log('MONACO - onChange for actual editor', newValue, e);
-
-        //console.log("monacoEditor: ", monacoEditor);
-
-        //console.log('editor selections', monacoEditor.getSelection());
+    _onChange: function() {
+        console.log("SceneMonacoTextEditor - _onChange [ react based update ]");
     },
 
     onTextSelection: function(e) {
         console.log("On Text Selection: ", e);
     },
+
+    saveJSON: function() {
+        return function() {
+
+            if(!monacoEditor) {
+                console.log("Nothing to save yet as component not mounted");
+                return;
+            }
+
+            try {
+                var newValue = monacoEditor.getValue();
+                // parse it and see if it blows up
+                var newScene = JSON.parse(newValue);
+                // make sure that something changed
+
+                var shouldSave = false;
+                _.forEach(this.props.scene.scene, function(sceneObj){
+                    if(!shouldSave) {
+                        shouldSave = ! sceneObj.hasOwnProperty("_id");
+                    }
+                });
+
+
+                if (! _.isEqual(this.props.scene, newScene) || shouldSave) { //TODO ensure a save occurs for scene media without id - must update view with _id
+                    SceneActions.updateScene(newScene);
+                }
+
+                //this.setState({error: null});
+
+            } catch (e) {
+                if (e instanceof SyntaxError) {
+                    //this.setState({error: e.message});
+                } else {
+                    throw e;
+                }
+            }
+        }.bind(this);
+    },
+
+    onChange: function(newValue, e) {
+        var saveTimeout;
+
+        if (saveTimeout) clearTimeout(saveTimeout);
+
+        saveTimeout = setTimeout(this.saveJSON(false), 1000);
+    },
+
 
     onDidMount: function(editor, monaco) {
         console.log('MONACO - onDidMount', editor);
@@ -69,16 +111,46 @@ var SceneMonacoTextEditor = React.createClass({
         // monacoEditor.onDidChangeCursorSelection(this.onTextSelection)
     },
 
-    onDidUpdate: function(previousProps, previousState) {
-        console.log("MONACO - onDidUpdate");
-
-        this._onChange();
-    },
-
     componentDidUpdate: function(previousProps, previousState) {
         console.log("MONACO - componentDidUpdate");
 
-        this._onChange();
+        //TODO check if any errors
+
+        try {
+            var editorScene = JSON.parse(monacoEditor ? monacoEditor.getValue() : "{}"); //TODO might need changing
+
+            console.log("MONACO - componentDidUpdate - editorScene: ", editorScene);
+
+            if(! _.isEqual(editorScene, this.getHumanReadableScene())) {
+                monacoEditor.setValue(this.getSceneString()); //CHECK if we should set document to new json
+            } 
+        } catch (e) {
+            console.log("Error with bad json: ", e);
+        }
+
+
+        if(this.props.focusedMediaObject !== previousProps.focusedMediaObject) {
+            var sceneMediaObjectRegex = /{.*tags.*type.*\},/i;
+
+            //TODO APEP: Github issue raised about issue with regex.
+            //TODO APEP: https://github.com/Microsoft/monaco-editor/issues/216
+            // sceneMediaObjectRegex = /tags(.|[\s\S])*type/g;
+
+            //{[\s\S]{1,10}tags.*[\s\S]*?type[\s\S]*?}[\s\S]*?}
+
+            // sceneMediaObjectRegex = /{[\s\S]{1,10}tags.*[\s\S]*?type[\s\S]*?}[\s\S]*?}/g;
+
+            // sceneMediaObjectRegex = /tags.*?type/g;
+
+            sceneMediaObjectRegex = /tags[\s\S]*?cook/;
+
+            var matches = monacoEditor.getModel().findMatches(sceneMediaObjectRegex, false, true, false, false);
+
+            console.log("MATCHES: " , matches);
+
+            //TODO try focus here
+
+        }
     },
 
     render: function() {
