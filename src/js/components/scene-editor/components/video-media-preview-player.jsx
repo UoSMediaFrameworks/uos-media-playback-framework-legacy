b6b988f77936a312ca4be815071c1aea013dec65
Angel Petrov <angel.a.petrov2@gmail.com>
2016-12-08 10:45:29 +0000

    logs for information on the preview player and setting the autoplay for the raw player to false, in order to stop auto playing

--- src/js/components/scene-editor/components/video-media-preview-player.jsx
+++ src/js/components/scene-editor/components/video-media-preview-player.jsx
@@ -9,19 +9,17 @@ var VideoMediaPreviewPlayer = React.createClass({
         return scenes.scene[currentMediaItemIndex] || null;
     },
 
-    getInitialState: function() {
-        return {
-
-        };
+    getInitialState: function () {
+        return {};
     },
 
-    _getVimeoPlayerForMediaObject: function(mediaObject) {
+    _getVimeoPlayerForMediaObject: function (mediaObject) {
         var vimeoId = getVimeoId(mediaObject.url);
         var url = 'https://player.vimeo.com/video/' + vimeoId;
         return <iframe ref="videoPlayer" width="100%" height="100%" src={url}></iframe>;
     },
 
-    _getMediaObjectUrl: function(mediaObject) {
+    _getMediaObjectUrl: function (mediaObject) {
         var dashUrl = mediaObject.url.replace("raw", "transcoded/dash");
 
         var trailingSlash = dashUrl.lastIndexOf("/");
@@ -38,28 +36,32 @@ var VideoMediaPreviewPlayer = React.createClass({
      * Ultimately, we would need websockets and a store for all the video media objects
      * @returns {XML: Video HTML component}
      */
-    _getDashPlayer: function(mediaObject) {
+    _getDashPlayer: function (mediaObject) {
         console.log("getting dash player", this._getMediaObjectUrl(mediaObject))
-        var dashUrl =  this._getMediaObjectUrl(mediaObject)
+        var dashUrl = this._getMediaObjectUrl(mediaObject)
         // dashUrl = "Transcoding_3/video_manifest.mpd";  //APEP: Use dash transcoded from a local express file path (dist folder)  CORS issue as azure storage account does not like localhost:port
-        return <video ref="videoPlayer" data-dashjs-player id="example-video"  width="100%" height="320px" controls>
+        var video = <video data-dashjs-player id="videoPreview" width="100%" height="320px" controls>
             <source src={dashUrl} type="application/dash+xml"></source>
         </video>;
+        console.log("_getDashPlayer",video)
+        return  video;
     },
 
     /**
      * Provides standard video playback of raw uploaded file
      * @returns {XML: Video HTML component}
      */
-    _getFilePlayer: function(mediaObject) {
+    _getFilePlayer: function (mediaObject) {
         return <video width="100%" height="100%" controls>
             <source src={mediaObject.url}></source>
         </video>;
     },
 
-    _getRawPlayerForMediaObject: function(mediaObject) {
+    _getRawPlayerForMediaObject: function (mediaObject) {
 
-        return this._getDashPlayer(mediaObject);
+        var test = this._getDashPlayer(mediaObject);
+        console.log("_getRawPlayerForMediaObject",test)
+        return test;
 
         // APEPE: We do not have the video media object database value at this point
         // Ultimately, we would need websockets and a store for all the video media objects
@@ -69,39 +71,42 @@ var VideoMediaPreviewPlayer = React.createClass({
         // return this._getFilePlayer(mediaObject);
     },
 
-    getVideoPlayerForMediaObject: function(mediaObject) {
-        if(!mediaObject) {
+    getVideoPlayerForMediaObject: function (mediaObject) {
+        if (!mediaObject) {
             return;
         }
 
-        if(mediaObject.url.indexOf('vimeo.com') != -1) {
+        if (mediaObject.url.indexOf('vimeo.com') != -1) {
             return this._getVimeoPlayerForMediaObject(mediaObject);
         }
 
         return this._getRawPlayerForMediaObject(mediaObject);
     },
 
-    componentDidUpdate: function() {
+    componentDidUpdate: function () {
 
         var mediaObject = this._getMediaObject(this.props);
 
-        if(! mediaObject){
+        if (!mediaObject) {
             return;
         }
 
         var url = this._getMediaObjectUrl(mediaObject);
         // var url = "Transcoding_3/video_manifest.mpd"; //APEP see other comment (_getRawPlayerForMediaObject)
         var player = dashjs.MediaPlayer().create();
-        player.initialize(document.querySelector("#example-video"), url, true);
+        player.initialize(document.querySelector("#videoPreview"), url, true);
+        var raw_player = document.querySelector("#videoPreview");
+        console.log("player-preview", raw_player)
+        if (raw_player)
+            raw_player.autoplay = false;
     },
 
-    render: function() {
+    render: function () {
 
         var mediaObject = this._getMediaObject(this.props);
 
         var video = this.getVideoPlayerForMediaObject(mediaObject);
-        console.log("player-preview",this)
-        //this.refs.videoPlayer.autoplay = false;
+        console.log("returned video",video)
 
         return (
             <div style={{width: '100%', height: '320px'}}>
