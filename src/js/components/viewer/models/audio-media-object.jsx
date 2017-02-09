602e1500b7b3acedbaf54a23a0594decdabbf07c
aaronphillips <aaronp261@gmail.com>
2017-02-09 21:54:26 +0000

    Audio Replay feature added, we now can allow autoreplay on audio sources.
    - Default is set to 1.
    - TODO autoreplay 0 does not look like it has been used at any point in previous audio development so still to be added.

--- src/js/components/viewer/models/audio-media-object.jsx
+++ src/js/components/viewer/models/audio-media-object.jsx
@@ -9,11 +9,19 @@ var AudioMediaObject = React.createClass({
 
     getInitialState: function () {
         return {
-            playing: true
+            playing: true,
+            autoreplay: 0,
+            currentLoop: 0
         };
     },
 
     componentDidMount: function() {
+        // APEP set the autoreply value based off the mediaObject, with an overriding default of 1
+        if(this.props.data && this.props.data.mediaObject && this.props.data.mediaObject._obj) {
+             var autoreplay = this.props.data.mediaObject._obj.hasOwnProperty("autoreplay") ? this.props.data.mediaObject._obj.autoreplay : 1;
+            this.setState({"autoreplay": autoreplay})
+        }
+
         this.play();
     },
 
@@ -27,6 +35,36 @@ var AudioMediaObject = React.createClass({
             .onUpdate(function() {
                 self.state.player.volume(position.vol);
             });
+    },
+
+    unlockCuePointsForMediaObject: function(mediaObject){
+        // Ensure that the cue point events are unlocked - the source of locking is after they've triggered within the time handler
+        // We unlock them at the start of play to allow them to be triggered again
+        var cues = mediaObject.cuePointEvents || [];
+        for (var i = 0; i < cues.length; i++) {
+            if(cues[i].locked){
+                cues[i].locked = false;
+            }
+        }
+    },
+
+    // APEP looping handler to determine autoreplay values
+    loopingHandler: function() {
+        var self = this;
+
+        this.setState({currentLoop: this.state.currentLoop + 1});
+
+        if(this.state.currentLoop >= this.state.autoreplay) {
+            self.transition();
+        } else {
+            console.log("AudioMediaObject - loopingHandler - Loop again - currentLoop: , autoreplay: ", this.state.currentLoop, this.state.autoreplay);
+            self.state.player.seek(0);
+            self.state.player.play();
+            var mediaObject = self.props.data.mediaObject._obj;
+            self.unlockCuePointsForMediaObject(mediaObject);
+
+        }
+
     },
 
     play: function() {
@@ -55,54 +93,57 @@ var AudioMediaObject = React.createClass({
                 throw_errors: false,
                 format_time: false,
                 ready: function() {
-                    if(self.state.playing) {
-                        this.load(streamUrl);
-                        this.volume(0);
-                        var cues = mediaObject.cuePointEvents || [];
-                        for (var i = 0; i < cues.length; i++) {
-                            if(cues[i].locked){
-                                cues[i].locked = false;
+                    if(!self.state.playing) {
+                        return;
+                    }
+
+                    this.load(streamUrl);
+                    this.volume(0);
+
+                    self.unlockCuePointsForMediaObject(mediaObject);
+
+                    this.play();
+
+                    var _ops = self.props.data.mediaObject._ops;
+
+                    //APEP tween volume
+                    self.tweenAudio(0, volume, _ops.transitionDuration).start();
+
+                    var transitionSeconds = (_ops.transitionDuration || 10) / 1000;
+
+                    var cues = mediaObject.cuePointEvents || [];
+
+                    this.on('timeupdate', function (position, duration) {
+                        if (cues.length > 0) {
+                            for (var i = 0; i < cues.length; i++) {
+                                try {
+                                    //We need to turn the time to seconds not miliseconds
+                                    var cue = cues[i];
+                                    if (position >= cue.timeElapsed && position < cue.timeElapsed + 1) {
+                                        if (cue.locked == undefined) {
+                                            cue.locked = false;
+                                        }
+                                        if (!cue.locked) {
+                                            cue.locked = true;
+                                            console.log("audio-media-object cue - cue.timeElapsed: , cue.locked: ", cue.timeElapsed, cue.locked);
+                                            self.props.data.triggerMediaActiveTheme(cue.themes);
+                                        }
+                                        cues[i] = cue;
+                                    }
+                                } catch (e) {
+                                    console.log("err", e)
+                                }
                             }
                         }
-                        this.play();
 
-                        var _ops = self.props.data.mediaObject._ops;
+                        if ((duration - position) < transitionSeconds || duration < transitionSeconds) {
+                            self.loopingHandler();
+                        }
+                    });
 
-                        //APEP tween volume
-                        self.tweenAudio(0, volume, _ops.transitionDuration).start();
-
-                        var transitionSeconds = (_ops.transitionDuration || 10) / 1000;
-
-                        this.on('timeupdate', function (position, duration) {
-                            if (cues != []) {
-                                for (var i = 0; i < cues.length; i++) {
-                                    try {
-                                        //We need to turn the time to seconds not miliseconds
-                                        var cue = cues[i];
-                                        if (position >= cue.timeElapsed && position < cue.timeElapsed + 1) {
-                                            if (cue.locked == undefined) {
-                                                cue.locked = false;
-                                            }
-                                            if (!cue.locked) {
-                                                cue.locked = true;
-                                                console.log("audio-media-object cue");
-                                                self.props.data.triggerMediaActiveTheme(cue.themes);
-                                            }
-                                            cues[i] = cue;
-                                        }
-                                    } catch (e) {
-                                        console.log("err", e)
-                                    }
-                                }
-                                if ((duration - position) < transitionSeconds || duration < transitionSeconds) {
-                                    self.transition();
-                                }
-                            }
-                        });
-                        this.on('error', function(err) {
-                            self.transition();
-                        });
-                    }
+                    this.on('error', function(err) {
+                        self.loopingHandler();
+                    });
                 }
             })
         });
@@ -124,6 +165,8 @@ var AudioMediaObject = React.createClass({
                 self.tweenAudio(self.state.player.volume(), 0, _ops.transitionDuration)
                     .onComplete(function() {
                         self.state.player.pause();
+                        // APEP Ensure the Audio5 component cleans up
+                        self.state.player.destroy();
                         self.props.data.moDoneHandler(self);
                     })
                     .start();
