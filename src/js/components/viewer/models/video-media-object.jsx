d956a5b9942d8cb7a010c4ae4c9787a587ceaa7b
aaronphillips <aaronp261@gmail.com>
2017-03-28 22:25:22 +0100

    Bug fix for applying default size.

--- src/js/components/viewer/models/video-media-object.jsx
+++ src/js/components/viewer/models/video-media-object.jsx
@@ -120,8 +120,10 @@ var VideoMediaObject = React.createClass({
                                 console.log("largestVideoAdaptationSet: ", largestVideoAdaptationSet);
 
                                 var element = self.refs[self.props.data.mediaObject._obj._id];
-                                element.clientWidth = largestVideoAdaptationSet.width;
-                                element.clientHeight = largestVideoAdaptationSet.height;
+                                
+                                //element.clientWidth = largestVideoAdaptationSet.width;
+                                //element.clientHeight = largestVideoAdaptationSet.height;
+                                
                                 var style = {};
 
                                 if(largestVideoAdaptationSet.height > largestVideoAdaptationSet.width) {
