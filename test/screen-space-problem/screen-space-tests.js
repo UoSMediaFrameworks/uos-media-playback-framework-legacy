69022f659563d44fe57c684df737080f668b255d
Aaron Phillips <aaronp261@gmail.com>
2017-06-02 18:07:54 +0100

    Extended some testing for the ScreenSpaceNode class
    - Implemented a basic Vector2 class - this should really be replaced with a full implementation that is unit tested and was only implemented due to no wifi and its used as a basic wrapper for x and y
    
    - Began the implementation and testing of a ScreenSpaceManager that uses the above two classes to provide the required functionality expected for the graph screen space problem.
    - The class is not fully implemented but has a good start with comments and some tests ready to pick up

--- test/screen-space-problem/screen-space-tests.js
+++ test/screen-space-problem/screen-space-tests.js
@@ -170,43 +170,24 @@ describe('ScreenSpace Testing', function () {
 
             assert.equal(ssNode.canFitNewElement(16,16), false, "");
         });
+
+        it("An empty 32x32 node can contain 64 4x4 nodes and no others", function() {
+            var ssNode = new ScreenSpaceNode(0, 0, 32, 32);
+
+            for(var i = 0; i < 64; i++) {
+                assert.equal(ssNode.canFitNewElement(4,4), true, "");
+                ssNode.placeNewElement(4,4);
+            }
+
+            assert.equal(ssNode.canFitNewElement(32,32), false, "");
+            assert.equal(ssNode.canFitNewElement(16,16), false, "");
+            assert.equal(ssNode.canFitNewElement(8,8), false, "");
+            assert.equal(ssNode.canFitNewElement(4,4), false, "");
+        });
+
+        // APEP TODO write some tests for randomness and implement
     });
 });
-
-// APEP manual testing start //
-
-// var topA = new ScreenSpaceNode(0, 0, 32, 32);
-// topB = new ScreenSpaceNode(32, 0, 32, 32),
-// bottomA = new ScreenSpaceNode(0, 32, 32, 32),
-// bottomB = new ScreenSpaceNode(32, 32, 32, 32);
-
-// var result = topA.placeNewElement(16, 16);
-// console.log("result 1: offsetX: " + result.offsetX + ", offsetY: " + result.offsetY);
-// // console.log("result: ", result);
-//
-// result = topA.placeNewElement(4,4);
-// console.log("result 2: offsetX: " + result.offsetX + ", offsetY: " + result.offsetY);
-//
-// result = topA.placeNewElement(4,4);
-// console.log("result 3: offsetX: " + result.offsetX + ", offsetY: " + result.offsetY);
-//
-// if(topA.canFitNewElement(16, 16)) {
-//     result = topA.placeNewElement(16,16);
-//     console.log("result 4: offsetX: " + result.offsetX + ", offsetY: " + result.offsetY);
-// } else {
-//     console.log("Cannot fit result 4");
-// }
-//
-// if(topA.canFitNewElement(16, 16)) {
-//     result = topA.placeNewElement(16,16);
-//     console.log("result 5: offsetX: " + result.offsetX + ", offsetY: " + result.offsetY);
-// } else {
-//     console.log("Cannot fit result 5");
-// }
-//
-// fs.writeFile('myjsonfile.json', JSON.stringify(topA), 'utf8');
-
-// APEP END manual testing start //
 
 // APEP BELOW WORKS BEFORE RANDOMISATION - IE PICKS FIRST AVAILABLE
 //
@@ -245,21 +226,131 @@ describe('ScreenSpace Testing', function () {
 //     return answer
 // }
 
+class Vector2 {
+
+    constructor(x, y) {
+        this.x = x || 0;
+        this.y = y || 0;
+    }
+
+    static identity() {
+        return new Vector2(0, 0);
+    }
+
+    print() {
+        console.log("Vector2 x: " + this.x + ", y: " + this.y);
+    }
+}
+
+describe("Vector2", function() {
+    describe("Constructor Tests", function() {
+        it("A vector2 can be defined with the x,y values can be retrieved", function() {
+            var vecTest = new Vector2(5,10);
+            assert.equal(vecTest.x, 5);
+            assert.equal(vecTest.y, 10);
+        });
+
+        it("A vector2 can be defined with no params leading to Vector2.identity", function() {
+            var vecTest = new Vector2();
+            assert.equal(vecTest.x, 0);
+            assert.equal(vecTest.y, 0);
+        });
+
+        it("static .identity provides a new instance with 0, 0", function() {
+            var vecTest = Vector2.identity();
+            assert.equal(vecTest.x, 0);
+            assert.equal(vecTest.y, 0);
+        });
+
+    });
+});
+
+class ScreenSpaceManager {
 
 
-class ScreenSpace {
+    constructor(vecScreenDim) {
+        this._screenSpaceNodeDim = new Vector2(32, 32);
+        this.vecScreenDim = vecScreenDim;
+        // APEP a list if a pretty poor data structure choice for this problem, however, I'll keep it for now
+        this.nodes = [];
+        this._growScreenSpaceNodesTopLeftRowsThenColumns();
+    }
 
-    constructor() {
-        var nodes = [];
+    // APEP We could make optional the ability to grow the space nodes around the centre point or top,bottom,left,right
+    _growScreenSpaceNodesTopLeftRowsThenColumns() {
+        var xCols = this.vecScreenDim.x / this._screenSpaceNodeDim.x;
+        var yCols = Math.floor(this.vecScreenDim.y / this._screenSpaceNodeDim.y);
+
+        this._screenSpaceNodeDim.print();
+        this.vecScreenDim.print();
+
+        for(var y = 0; y < yCols; y++) {
+            for(var x = 0; x < xCols; x++) {
+                this.nodes.push(new ScreenSpaceNode(x * this._screenSpaceNodeDim.x, y * this._screenSpaceNodeDim.y, this._screenSpaceNodeDim.x, this._screenSpaceNodeDim.y));
+            }
+        }
     }
 
     // proportion of X, Y
     getStaticPosition(globalOffsetX, globalOffsetY) {
 
+        // APEP allow static positions to be used or searched
+
     }
 
-    searchRegionNearNode(node) {
+    searchRegionNearNode(node, regionScope) {
 
+        // APEP given node, use regionScope (int or vector) to grow out a region in which we search through out data structure for nodes
+
+        return [];
+    }
+
+    // APEP helper function to find nodes that
+    getNodesNeighbours(node) {
+
+        return [];
+    }
+
+    // APEP helper function for finding nodes
+    getNode(offsetX, offsetY) {
+        return _.find(this.nodes, function(node){
+           return node.offsetX === offsetX && node.offsetY;
+        });
     }
 
 }
+
+describe("ScreenSpaceManager", function() {
+    describe("Constructor tests", function() {
+        it("Given a screen dimension, the space will be divided into a matrix style data structure full of screen space nodes", function() {
+            var ssManager = new ScreenSpaceManager(new Vector2(64,32));
+            assert.equal(ssManager.nodes.length, 2, "2 32x32 nodes should be placeble");
+        });
+
+        it("Given a screen dimension, 2 rows of 2 columns should be created", function() {
+            var ssManager = new ScreenSpaceManager(new Vector2(64,64));
+            assert.equal(ssManager.nodes.length, 4, "4 32x32 nodes should be placeble");
+        });
+
+        it("Given a 1080p screen size, the space will result in 1980 32x32 nodes", function() {
+            var ssManager = new ScreenSpaceManager(new Vector2(1920, 1080));
+            assert.equal(ssManager.nodes.length, 1980);
+        });
+    });
+
+    describe("searchRegionNearNode tests", function() {
+        it("Given a manager with a 10x10 grid setup, we can search for the 9 top left nodes, given the node for test is correctly picked", function() {
+            var ssManager = new ScreenSpaceManager(new Vector2(320, 320));
+            var knownNodeForSearch = ssManager.getNode(32, 32);
+            assert.equal(ssManager.searchRegionNearNode(knownNodeForSearch, 1).length, 9);
+        });
+    });
+
+    describe("getNodesNeighbours tests", function() {
+        it("Given a manager with a 10x10 grid setup, we can search the neighbours (4) of a node, given the node is not an outer node", function() {
+            var ssManager = new ScreenSpaceManager(new Vector2(320, 320));
+            var knownNodeForSearch = ssManager.getNode(32, 32);
+            assert.equal(ssManager.getNodesNeighbours(knownNodeForSearch).length, 4);
+        });
+    });
+});
