9bd24b97087754fb7d3ebae566da185e57314ba2
Aaron Phillips <aaronp261@gmail.com>
2017-10-20 12:25:31 +0100

    Remove random character?

--- src/js/components/pages/adjustable-grid.jsx
+++ src/js/components/pages/adjustable-grid.jsx
@@ -218,7 +218,7 @@ var RespGrid = React.createClass({
         }
     },
     removeComponent: function (item) {
-        SceneActions.removeLayoutComponent(item.i);g
+        SceneActions.removeLayoutComponent(item.i);
     },
     render: function () {
         var self = this;
