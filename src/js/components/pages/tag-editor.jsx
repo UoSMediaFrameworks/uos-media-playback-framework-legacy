a70994b03f3c72601c3520a92d2ed4cbf089e7e3
Aaron Phillips <aaronp261@gmail.com>
2017-11-20 12:55:55 +0000

    Reformat file

--- src/js/components/pages/tag-editor.jsx
+++ src/js/components/pages/tag-editor.jsx
@@ -18,14 +18,14 @@ var TagEditor = React.createClass({
 //lifecycle
 
     getInitialState: function () {
-      return {
-        scene: SceneStore.getScene(this.props._id),
-        focusedMediaObject: null,
-        shouldSave: false,
-        tags: [],
-        suggestions: [],
-        mediaObjectSelected: false
-      };
+        return {
+            scene: SceneStore.getScene(this.props._id),
+            focusedMediaObject: null,
+            shouldSave: false,
+            tags: [],
+            suggestions: [],
+            mediaObjectSelected: false
+        };
     },
 
     componentDidMount: function () {
@@ -43,12 +43,12 @@ var TagEditor = React.createClass({
         GridStore.removeChangeListener(this._onFocusChange)
     },
 
-    componentDidUpdate: function() {
-      //schedules a save in 1 second. Avoids to many saves
-      if (this.state.shouldSave) {
-        this.setState({shouldSave: false})
-        this.setSave()
-      }
+    componentDidUpdate: function () {
+        //schedules a save in 1 second. Avoids to many saves
+        if (this.state.shouldSave) {
+            this.setState({shouldSave: false})
+            this.setSave()
+        }
     },
 
 //autosave functions
@@ -63,25 +63,25 @@ var TagEditor = React.createClass({
 
     saveToScene() {
 
-      var tags = [];
+        var tags = [];
 
-      if (this.state.tags.length > 0) {
-        for (var i = 0; i < this.state.tags.length; i++) {
-          tags.push(this.state.tags[i].text)
+        if (this.state.tags.length > 0) {
+            for (var i = 0; i < this.state.tags.length; i++) {
+                tags.push(this.state.tags[i].text)
+            }
         }
-      }
 
-      var csv = tags.toString();
-      console.log(csv);
+        var csv = tags.toString();
+        console.log(csv);
 
-      try {
-          var scene = this.state.scene
-          scene.scene[this.state.focusedMediaObject].tags = csv;
-          SceneActions.updateScene(scene)
-          console.log("TagEditor: Changes saved");
-      } catch (error) {
-          console.log("TagEditor: Failed to save changes:", error)
-      }
+        try {
+            var scene = this.state.scene
+            scene.scene[this.state.focusedMediaObject].tags = csv;
+            SceneActions.updateScene(scene)
+            console.log("TagEditor: Changes saved");
+        } catch (error) {
+            console.log("TagEditor: Failed to save changes:", error)
+        }
 
     },
 
@@ -89,133 +89,137 @@ var TagEditor = React.createClass({
 
     componentWillReceiveProps: function (nextProps) {
     },
-      
 
 
-    getNewState: function() {
-      var scene = SceneStore.getScene(this.props._id)
-      var focusedMediaObject = GridStore.getFocusedMediaObject();
+    getNewState: function () {
+        var scene = SceneStore.getScene(this.props._id)
+        var focusedMediaObject = GridStore.getFocusedMediaObject();
 
-      var mediaObject = null;
-      var tags = [];
+        var mediaObject = null;
+        var tags = [];
 
-      if (focusedMediaObject != null) {
+        if (focusedMediaObject != null) {
 
-      //have to use try catch because focusedMediaObject can be out of bounds
-      try {
-        mediaObject = scene.scene[focusedMediaObject]
-      } catch (error) {
-        console.log("TagEditor: failed to read media object", error);
-      }
-
-      }
-
-      if (mediaObject != null) {
-
-
-      //get the tags from the scene object
-      var mediaObjectTags = mediaObject.tags.split(",");
-
-      //get rid of spaces and format for tag editor
-      if (mediaObjectTags.length >= 1) {
-        for (var i = 0; i < mediaObjectTags.length; i++) {
-          mediaObjectTags[i] = mediaObjectTags[i].trim();
-
-          //add tags as long as they are not empty
-          if (mediaObjectTags[i] != "") {
-            tags.push({id: i, text: mediaObjectTags[i].trim()})
-          }
+            //have to use try catch because focusedMediaObject can be out of bounds
+            try {
+                mediaObject = scene.scene[focusedMediaObject]
+            } catch (error) {
+                console.log("TagEditor: failed to read media object", error);
+            }
 
         }
+
+        if (mediaObject != null) {
+
+
+            //get the tags from the scene object
+            var mediaObjectTags = mediaObject.tags.split(",");
+
+            //get rid of spaces and format for tag editor
+            if (mediaObjectTags.length >= 1) {
+                for (var i = 0; i < mediaObjectTags.length; i++) {
+                    mediaObjectTags[i] = mediaObjectTags[i].trim();
+
+                    //add tags as long as they are not empty
+                    if (mediaObjectTags[i] != "") {
+                        tags.push({id: i, text: mediaObjectTags[i].trim()})
+                    }
+
+                }
+            }
         }
-      }
-      //update display
-      this.setState({scene: scene, suggestions: this.getSceneTags(scene), focusedMediaObject: focusedMediaObject, tags: tags});
-      
+        //update display
+        this.setState({
+            scene: scene,
+            suggestions: this.getSceneTags(scene),
+            focusedMediaObject: focusedMediaObject,
+            tags: tags
+        });
+
     },
 
 
     _onChange: function () {
-      this.getNewState();
+        this.getNewState();
     },
 
     _onFocusChange: function () {
-      this.getNewState();
+        this.getNewState();
     },
 
-    getSceneTags: function(scene) {
+    getSceneTags: function (scene) {
         if (scene != null) {
-          var tags = []
+            var tags = []
 
-          for (var i = 0; i < scene.scene.length; i++) {
+            for (var i = 0; i < scene.scene.length; i++) {
 
-              //pull tags from each media object and split down
-              var newtags = scene.scene[i].tags.split(",");
-              for (var j = 0; j < newtags.length; j++) {
+                //pull tags from each media object and split down
+                var newtags = scene.scene[i].tags.split(",");
+                for (var j = 0; j < newtags.length; j++) {
 
-                //get rid of spaces
-                var newtag = newtags[j].trim()
+                    //get rid of spaces
+                    var newtag = newtags[j].trim()
 
-                //add tag only if it dosn't already exist
-                tags.indexOf(newtag) === -1 ? tags.push(newtag) : console.log("Tag Already Exists")
-              }
-          }
-          console.log("Tags", tags);
-          return tags;
+                    //add tag only if it dosn't already exist
+                    tags.indexOf(newtag) === -1 ? tags.push(newtag) : console.log("Tag Already Exists")
+                }
+            }
+            console.log("Tags", tags);
+            return tags;
         } else {
-          return [];
+            return [];
         }
     },
 
 //tag editor callbacks
 
-    handleDelete: function(i) {
-       let tags = this.state.tags;
-       tags.splice(i, 1);
-       this.setState({tags: tags, shouldSave: true});
+    handleDelete: function (i) {
+        let tags = this.state.tags;
+        tags.splice(i, 1);
+        this.setState({tags: tags, shouldSave: true});
     },
 
-    handleAddition: function(tag) {
-       let tags = this.state.tags;
-       tags.push({
-           id: tags.length + 1,
-           text: tag
-       });
-       this.setState({tags: tags, shouldSave: true});
+    handleAddition: function (tag) {
+        let tags = this.state.tags;
+        tags.push({
+            id: tags.length + 1,
+            text: tag
+        });
+        this.setState({tags: tags, shouldSave: true});
     },
 
-    handleDrag: function(tag, currPos, newPos) {
-       let tags = this.state.tags;
+    handleDrag: function (tag, currPos, newPos) {
+        let tags = this.state.tags;
 
-       // mutate array
-       tags.splice(currPos, 1);
-       tags.splice(newPos, 0, tag);
+        // mutate array
+        tags.splice(currPos, 1);
+        tags.splice(newPos, 0, tag);
 
-       // re-render
-       this.setState({ tags: tags, shouldSave: true});
-     },
+        // re-render
+        this.setState({tags: tags, shouldSave: true});
+    },
 
     render: function () {
 
-      if (this.state.focusedMediaObject == null) {
+        if (this.state.focusedMediaObject == null) {
+            return (
+                <div>Please select a media object</div>
+            )
+        }
+
         return (
-          <div>Please select a media object</div>
+            <div>
+
+                <ReactTags
+                    tags={this.state.tags}
+                    suggestions={this.state.suggestions}
+                    handleDelete={this.handleDelete}
+                    handleAddition={this.handleAddition}
+                    handleDrag={this.handleDrag}
+                />
+            </div>
+
         )
-      }
-
-      return(
-        <div>
-
-          <ReactTags
-            tags={this.state.tags}
-            suggestions={this.state.suggestions}
-            handleDelete={this.handleDelete}
-            handleAddition={this.handleAddition}
-            handleDrag={this.handleDrag}
-          />
-        </div>
-
-      )
     }
 
 });
