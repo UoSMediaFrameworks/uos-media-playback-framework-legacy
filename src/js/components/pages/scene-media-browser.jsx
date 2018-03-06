8fc01fa03f716f8b1ed1cabfb9867a4e4bd62969
Matthew Kotara <matthewkotara@gmail.com>
2018-03-06 14:42:11 +0000

    Media browser changes: 1. Fix list CSS 2. Fix lower pane height ajustment 3. Make filter the default search 4. Update button text to be more descriptive 5. Move preview to the top and make resizeable 6. Remove tag's union as will be replicated in tag editor 7. Remove save flag (replication) 8. Remove delete scene handler as no longer in use

--- src/js/components/pages/scene-media-browser.jsx
+++ src/js/components/pages/scene-media-browser.jsx
@@ -6,23 +6,29 @@ var GridStore = require("../../stores/grid-store");
 var AddMediaObjectStore = require("../../stores/add-media-object-store");
 var HubSendActions = require('../../actions/hub-send-actions');
 var SceneActions = require('../../actions/scene-actions');
-var TagUnion = require('../tag-union.jsx');
 var MediaObjectList = require('../scene-editor/media-object-list.jsx');
-var AddMediaObject = require('../scene-editor/add-media-object.jsx');
 var Loader = require('../loader.jsx');
 var MediaPreviewComponent =  require('../scene-editor/media-preview-player.jsx');
+var SplitPane = require('react-split-pane');
+var Measure = require('react-measure');
 
 var SceneMediaBrowser = React.createClass({
 
     getInitialState: function () {
         return _.extend(this.getStateFromStores(), {
-            focusedMediaObject: null
+            focusedMediaObject: null,
+            dimensions: {
+                width: -1,
+                height: -1
+              },
+            lowerPaneHeight: 0               
         });
     },
 
     _onChange: function () {
         this.setState(this.getStateFromStores());
     },
+
     _onFocusChange:function(){
         this.setState({focusedMediaObject:GridStore.getFocusedMediaObject()});
     },
@@ -34,12 +40,14 @@ var SceneMediaBrowser = React.createClass({
             savedStatus: SceneSavedStore.getSceneSaved()
         };
     },
+
     componentDidMount: function () {
         SceneStore.addChangeListener(this._onChange);
         SceneSavedStore.addChangeListener(this._onChange);
         GridStore.addChangeListener(this._onFocusChange);
         AddMediaObjectStore.addChangeListener(this._onChange);
     },
+
     componentWillUnmount: function () {
         SceneStore.removeChangeListener(this._onChange);
         SceneSavedStore.removeChangeListener(this._onChange);
@@ -47,17 +55,9 @@ var SceneMediaBrowser = React.createClass({
         AddMediaObjectStore.removeChangeListener(this._onChange);
     },
 
-    deleteSceneHandler: function(event) {
-        if (confirm('Deleting a scene will remove all associated images and tags.\n\nAre you sure?')) {
-            HubSendActions.deleteScene(this.state.scene._id);
-        }
-    },
-
     render: function () {
-
-        var saveFlagKlass = this.state.savedStatus ? "green-save-flag" : "red-save-flag";
-
         var showOverlay = this.state.uploading ? "show-overlay-when-uploading" : "hide-overlay-when-uploading";
+
         if (this.props._id == null || this.state.scene==null) {
             return (
                 <div className="mf-empty-grid-component">
@@ -65,6 +65,7 @@ var SceneMediaBrowser = React.createClass({
                 </div>
             );
         }
+
         return (
             <div className='flex-container monaco-editor vs-dark'>
                 <div className={showOverlay}></div>
@@ -72,22 +73,51 @@ var SceneMediaBrowser = React.createClass({
                 </div>
 
                 <div className="thumbs-and-json">
-                    <div className="flex-container">
-                        <MediaObjectList focusedMediaObject={this.props.focusedMediaObject}
-                                         focusHandler={SceneActions.changeMediaObjectFocus}
-                                         scene={this.state.scene} saveFlagKlass={saveFlagKlass}/>
-
-                        <MediaPreviewComponent focusedMediaObject={this.props.focusedMediaObject}
-                                               scene={this.state.scene}/>
-
-                    </div>
-
+                    <Measure onMeasure={this.onMeasure}>
+                        <SplitPane split="horizontal" minSize={0} defaultSize={this.getLastSplit()} onChange={size => this.splitChanged(size)}>
+                            <div style={{height: "100%", width: "100%"}}>
+                                <MediaPreviewComponent 
+                                    style={{height: "100%", width: "100%"}} 
+                                    focusedMediaObject={this.props.focusedMediaObject}
+                                    scene={this.state.scene}
+                                />
+                            </div>
+                            <div style={{height: (""+this.state.lowerPaneHeight+"px"), width: "100%"}}>
+                                <MediaObjectList 
+                                    focusedMediaObject={this.props.focusedMediaObject}
+                                    focusHandler={SceneActions.changeMediaObjectFocus}
+                                    scene={this.state.scene}
+                                />
+                            </div>
+                        </SplitPane>
+                    </Measure>
                 </div>
-                <TagUnion scene={this.state.scene} focusedMediaObject={this.state.focusedMediaObject}/>
             </div>
         )
-    }
+    },
 
+    splitChanged: function(size) {
+        //store size in local store
+        localStorage.setItem('splitPos', size)
+        var lowerPaneHeight = this.state.dimensions.height-size
+        this.setState({lowerPaneHeight: lowerPaneHeight})
+    },
+
+    getLastSplit: function() {
+        if (localStorage.getItem("splitPos") === null) {
+            //default 1/4 split.
+            return "25%"
+        } else { 
+            return parseInt(localStorage.getItem('splitPos'), 10)
+        }
+    },
+
+    onMeasure: function(dimensions) {
+        var topPaneHeight=parseInt(localStorage.getItem('splitPos'), 10)
+        var totalHeight = dimensions.height;
+        var lowerPaneHeight=totalHeight-topPaneHeight-10;
+        this.setState({dimensions: dimensions, lowerPaneHeight: lowerPaneHeight})
+    },
 
 });
 
