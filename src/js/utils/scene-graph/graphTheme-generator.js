746b05ea6a1b0d25d5999ed6b2c49835923caaf7
Petrov Angel <AAA48571@salford.ac.uk>
2018-03-06 14:05:01 +0000

    Improved logic so that the generation will happen only if the sceneGraph is empty

--- src/js/utils/scene-graph/graphTheme-generator.js
+++ src/js/utils/scene-graph/graphTheme-generator.js
@@ -3,23 +3,24 @@ var _ = require('lodash');
 
 module.exports = {
     generateGraphThemes: function (sceneGraph) {
-        console.log(sceneGraph)
         //AP: as disscussed this will take into account only one scene and this will be the first scene available
         var sceneKeys = Object.keys(sceneGraph.sceneIds);
         var scene = SceneStore.getScene(sceneKeys[0]);
-        if(scene){
+        if (scene) {
             var themeKeys = Object.keys(scene.themes);
-            console.log(themeKeys);
             //AP: We are assuming that this is using the structure of a sound graph type as written on 05/March/2018
             var root = sceneGraph.graphThemes.children["SOUNDS"];
-            _.each(themeKeys,function(key){
-                root.children[key] = {
-                    type: 'stheme',
-                    children: {}
-                }
-            })
+            if (Object.keys(root.children).length <1){
+                _.each(themeKeys, function (key) {
+                    root.children[key] = {
+                        type: 'stheme',
+                        children: {}
+                    }
+                })
+            }
+
+
         }
-        console.log("gen",sceneGraph);
         return sceneGraph;
     }
 };
