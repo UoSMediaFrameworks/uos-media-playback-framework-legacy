fcacd0260cc556439d4a802b54661b29d7dd8522
Aaron Phillips <aaronp261@gmail.com>
2018-09-05 12:09:02 +0100

    Added key missing dependencies
    Workbox js frustratingly wraps up some additional modular deps.
    A manual edit has been made to the dep in workbox-3.4.1/workbox-sw.js to point this modular loader at our local file serving.

--- src/sw-toolbox.js
+++ src/sw-toolbox.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+importScripts('external/workbox-cdn/releases/3.4.1/workbox-sw.js');
 
 if (workbox) {
     console.log(`Workbox dependency is supported by the browser`);
